81d0423c776d3e4fb85c62a980a7a8e3df8c0c2b
Danny Boy Loberanes Jr <146666264+Danny4122@users.noreply.github.com>
2024-05-28 04:42:51 +0800

    Update database.js

--- database.js
+++ database.js
@@ -2,7 +2,7 @@ const mongoose = require('mongoose');
 
 const connectDB = async () => {
   try {
-    await mongoose.connect('mongodb://192.168.1.22:27017/switchEvents', {
+    await mongoose.connect('mongodb://127.0.0.1/switchEvents', {
       useNewUrlParser: true,
       useUnifiedTopology: true,
     });

76bfead81ba193ad72aae6c96a22fcc559b8a7b5
Danny Boy Loberanes Jr <146666264+Danny4122@users.noreply.github.com>
2024-05-28 15:49:40 +0800

    Update server.js
    Working

--- server.js
+++ server.js
@@ -1,38 +1,69 @@
 const express = require('express');
 const bodyParser = require('body-parser');
-const mongoose = require('mongoose');
+// const mongoose = require('mongoose');
 const connectDB = require('./database');
 const SwitchEvent = require('./model/lightsModel'); // Ensure correct path to lightsModel.js
+const SwitchEventModel = require('./model/lightsModel');
 
 const app = express();
 const port = 3000;
 
 connectDB();
 
-mongoose.connect('mongodb://127.0.0.1/switchEvents', {
-  useNewUrlParser: true,
-  useUnifiedTopology: true,
-}).then(() => {
-  console.log('Connected to MongoDB');
-}).catch(err => {
-  console.error('Error connecting to MongoDB', err);
-});
 
 app.use(bodyParser.json());
 
+// app.get('/api/switchEvents', async (req, res) => {
+//   try {
+//     const events = await SwitchEvent.find().sort({ timestamp: -1 }); 
+//     res.json(events);
+//   } catch (error) {
+//     console.error(error);
+//     res.status(500).send('Error retrieving events');
+//   }
+// });
+
+// app.post('/api/switchEvents', async (req, res) => {
+//   try {
+//     const SwitchEvent = await new SwitchEvent({
+//       switch: req.body.switch,
+//       state: req.body.state,
+//       timestamp: new Date()
+//     });
+
+//     const result = await SwitchEvent.save();
+//     res.status(200).send(`Event inserted with ID: ${result._id}`);
+//   } catch (error) {
+//     console.error(error);
+//     res.status(500).send('Error inserting event', error);
+//     // console.error('Error inserting event:', error);
+//   }
+// });
+
+
+app.get('/api/switchEvents', async (req, res) => {
+  try {
+    const events = await SwitchEventModel.find().sort({ timestamp: -1 });
+    res.json(events);
+  } catch (error) {
+    console.error(error);
+    res.status(500).send('Error retrieving events');
+  }
+});
+
 app.post('/api/switchEvents', async (req, res) => {
   try {
-    const switchEvent = new SwitchEvent({
+    const newSwitchEvent = new SwitchEventModel({
       switch: req.body.switch,
       state: req.body.state,
       timestamp: new Date()
     });
 
-    const result = await switchEvent.save();
+    const result = await newSwitchEvent.save();
     res.status(200).send(`Event inserted with ID: ${result._id}`);
   } catch (error) {
     console.error(error);
-    res.status(500).send('Error inserting event');
+    res.status(500).send('Error inserting event', error);
   }
 });
 

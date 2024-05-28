e0e184a065d6c3ad2960bf4493f085b51909afc6
Danny Boy Loberanes Jr <146666264+Danny4122@users.noreply.github.com>
2024-05-28 17:05:56 +0800

    Update Project_Home_Automation.ino
    exact data result

--- Project_Home_Automation.ino
+++ Project_Home_Automation.ino
@@ -5,15 +5,21 @@
 const char* ssid = "LAN(D) OF JERUSALEM";
 const char* password = "@Isaiah65Eighteen**";
 
+bool lastSwitch1State = false;
+bool lastSwitch2State = false;
+unsigned long lastDebounceTimeSwitch1 = 0;
+unsigned long lastDebounceTimeSwitch2 = 0;
+const unsigned long debounceDelay = 200; // 200 ms debounce time
+
 void setup() {
   pinMode(D0, OUTPUT);
   pinMode(D1, OUTPUT);
   Serial.begin(9600);
-  delay(5000);
+  delay(500);
 
   WiFi.begin(ssid, password);
   while (WiFi.status() != WL_CONNECTED) {
-    delay(5000);
+    delay(500);
     Serial.println("Connecting to WiFi...");
   }
   Serial.println("Connected to WiFi");
@@ -31,44 +37,79 @@ void setup() {
 void loop() {
   ArduinoCloud.update();
 
-  if (WiFi.status() == WL_CONNECTED) {
-    sendSwitchState();
-  } else {
+  // Debounce and check switch 1 state
+  if ((millis() - lastDebounceTimeSwitch1) > debounceDelay) {
+    if (switch1 != lastSwitch1State) {
+      lastSwitch1State = switch1;
+      onSwitch1Change();
+      lastDebounceTimeSwitch1 = millis();
+    }
+  }
+
+  // Debounce and check switch 2 state
+  if ((millis() - lastDebounceTimeSwitch2) > debounceDelay) {
+    if (switch2 != lastSwitch2State) {
+      lastSwitch2State = switch2;
+      onSwitch2Change();
+      lastDebounceTimeSwitch2 = millis();
+    }
+  }
+
+  // If WiFi is not connected, inform the user
+  if (WiFi.status() != WL_CONNECTED) {
     Serial.println("WiFi not connected. Unable to send data to server");
   }
 }
 
-void sendSwitchState() {
-  WiFiClient client;
-  HTTPClient http;
+void sendSwitchState(String switchName, String state) {
+  if (WiFi.status() == WL_CONNECTED) {
+    static String lastSentStateSwitch1 = ""; // Add static variables to store last sent state
+    static String lastSentStateSwitch2 = "";
 
-  String url = "http://192.168.1.24:3000/api/switchevents"; // Ensure this is the correct IP and port
-  Serial.println("Connecting to URL: " + url);
+    // Check if the state has changed before sending
+    if ((switchName == "switch1" && state != lastSentStateSwitch1) ||
+        (switchName == "switch2" && state != lastSentStateSwitch2)) {
+      WiFiClient client;
+      HTTPClient http;
 
-  http.begin(client, url);
-  http.addHeader("Content-Type", "application/json");
+      String url = "http://192.168.1.24:3000/api/switchevents"; // Ensure this is the correct IP and port
+      Serial.println("Connecting to URL: " + url);
 
-  // Get the current timestamp
-  String timestamp = getTimestamp();
+      http.begin(client, url);
+      http.addHeader("Content-Type", "application/json");
 
-  // Properly format JSON data including switch1, switch2, state, and timestamp
-  String json = "{\"switch\": \"" + String(switch1 ? "switch1" : "switch2") + "\", \"state\": \"" + String(switch1 ? (switch1 ? "on" : "off") : (switch2 ? "on" : "off")) + "\", \"timestamp\": \"" + timestamp + "\"}";
+      // Get the current timestamp
+      String timestamp = getTimestamp();
 
-  Serial.println("JSON data: " + json);
-  
-  int httpResponseCode = http.POST(json);
+      // Properly format JSON data including switch name, state, and timestamp
+      String json = "{\"switch\": \"" + switchName + "\", \"state\": \"" + state + "\", \"timestamp\": \"" + timestamp + "\"}";
 
-  if (httpResponseCode > 0) {
-    String response = http.getString();
-    Serial.println("HTTP Response code: " + String(httpResponseCode));
-    Serial.println("Response: " + response);
+      Serial.println("JSON data: " + json);
+
+      int httpResponseCode = http.POST(json);
+
+      if (httpResponseCode > 0) {
+        String response = http.getString();
+        Serial.println("HTTP Response code: " + String(httpResponseCode));
+        Serial.println("Response: " + response);
+        
+        // Update last sent state
+        if (switchName == "switch1") {
+          lastSentStateSwitch1 = state;
+        } else if (switchName == "switch2") {
+          lastSentStateSwitch2 = state;
+        }
+      } else {
+        Serial.print("Error Sending data to server. HTTP Response code: ");
+        Serial.println(httpResponseCode);
+        Serial.println(http.errorToString(httpResponseCode));
+      }
+
+      http.end();
+    }
   } else {
-    Serial.print("Error Sending data to server. HTTP Response code: ");
-    Serial.println(httpResponseCode);
-    Serial.println(http.errorToString(httpResponseCode));
+    Serial.println("WiFi not connected. Unable to send data to server");
   }
-
-  http.end();
 }
 
 String getTimestamp() {
@@ -85,13 +126,12 @@ String getTimestamp() {
   String timestamp = "";
   char formattedTime[20];
   snprintf(formattedTime, 20, "%04lu-%02lu-%02luT%02lu:%02lu:%02luZ",
-           1970 + days / 365, 1 + (days % 365) / 30, 1 + (days % 365) % 30,
+           2024 + days / 365, 29 + (days % 365) / 30, 29 + (days % 365) % 30,
            hours % 24, minutes % 60, seconds % 60);
   timestamp = String(formattedTime);
   
   return timestamp;
 }
-
 
 void onSwitch1Change() {
   if (switch1) {
@@ -101,7 +141,7 @@ void onSwitch1Change() {
     digitalWrite(D0, HIGH);
     Serial.println("Switch1 turned OFF");
   }
-  sendSwitchState(); // Send state change to server
+  sendSwitchState("switch1", switch1 ? "on" : "off"); // Send state change to server
 }
 
 void onSwitch2Change() {
@@ -112,5 +152,5 @@ void onSwitch2Change() {
     digitalWrite(D1, HIGH);
     Serial.println("Switch2 turned OFF");
   }
-  sendSwitchState(); // Send state change to server
+  sendSwitchState("switch2", switch2 ? "on" : "off"); // Send state change to server
 }
